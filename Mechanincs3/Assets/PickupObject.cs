57c5581db127e35889ff2d46d7310d70cab7793e
Sherk-cell <22826@ma-web.nl>
2021-04-19 14:22:28 +0200

    first commit

---
+++ Mechanincs3/Assets/PickupObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupObject : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider collider)
+    {
+        if(collider.gameObject.tag == "Player")
+        {
+            print("picked up");
+            Destroy(gameObject);
+        }
+        
+    }
+}
---
+++ mechanincs4/Assets/blow_up.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blow_up : MonoBehaviour
+{
+    public GameObject particleEffect;
+    private ParticleSystem partSys;
+    private void Start()
+    {
+       // var particleMainSettings = partSys.main;
+       // particleMainSettings.loop ^= true;
+
+
+    }
+    void OnMouseUp()
+    {
+
+        //If your mouse hovers over the GameObject with the script attached, output this message
+        // partSys = GetComponent<ParticleSystem>();
+        // partSys.Stop();
+        GameObject boom = Instantiate(particleEffect, transform.position, Quaternion.identity);
+        //boom.GetComponent<ParticleSystem>();
+        Destroy(boom, 1.0f);
+       
+        Destroy(gameObject);
+        // partSys.Stop();
+        Debug.Log("works");      
+       // partSys.Play();
+
+    }
+
+  
+}
